c040a6e890ff3663c0710893183f8fc9919e6c04
Shreyaa1199 <shresthashreya813@gmail.com>
2025-11-02 23:36:51 +0545

    modified: user_cart.php

--- api/cart/user_cart.php
+++ api/cart/user_cart.php
@@ -6,7 +6,7 @@ $user_id = $_GET["user_id"] ?? null;
 
 if($user_id) {
     $stmt = $pdo->prepare("SELECT * FROM cart WHERE user_id = ?");
-    $stmt->execute([$user_id]);
+    $stmt->execute([':user_id' => $user_id]);
     echo json_encode($stmt->fetchAll());
 } else {
     echo json_encode(["message"=>"User ID missing"]);
